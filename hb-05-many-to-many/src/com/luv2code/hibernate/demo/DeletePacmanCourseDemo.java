1682232b74b7ec06464e5039e42f07f6e3494f0e
dddawiddd <dawidn@onet.eu>
2019-08-10 18:20:20 +0200

    27.13 @ManyToMany mapping. Relation between Courses and Students

---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/DeletePacmanCourseDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCoursesForMaryDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+
+        try {
+
+            //start a transaction
+            session.beginTransaction();
+
+            int tempId = 2;
+            Student tempStudent = session.get(Student.class, tempId);
+
+            System.out.println("\nLoaded student:" + tempStudent);
+            System.out.println("Courses:" + tempStudent.getCourses());
+
+            //commit transaction
+            session.getTransaction().commit();
+
+        } finally {
+
+            //add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/GetCoursesForMaryDemo.java
@@ -0,0 +1,58 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AddCoursesForMaryDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+
+        try {
+
+            //start a transaction
+            session.beginTransaction();
+
+            int tempId = 2;
+            Student tempStudent = session.get(Student.class, tempId);
+
+            System.out.println("\nLoaded student:" + tempStudent);
+            System.out.println("Courses:" + tempStudent.getCourses());
+
+            Course tempCourse1 = new Course("Rubik's Cube - How to speed Cube");
+            Course tempCourse2 = new Course("Atari 2600 - Game development");
+
+            tempCourse1.addStudent(tempStudent);
+            tempCourse2.addStudent(tempStudent);
+
+            session.save(tempCourse1);
+            session.save(tempCourse2);
+
+            //commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+
+            //add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
