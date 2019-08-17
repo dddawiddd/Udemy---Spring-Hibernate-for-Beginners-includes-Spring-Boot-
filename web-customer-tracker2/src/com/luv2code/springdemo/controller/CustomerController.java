023e3bba73f52f6fc7c4ef36f4672f9e846cd18c
dddawiddd <dawidn@onet.eu>
2019-08-17 15:09:18 +0200

    29.10 warking jsp page with controler and access to db

--- web-customer-tracker2/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker2/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,16 +1,32 @@
 package com.luv2code.springdemo.controller;
 
 
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    //Spring will scan for a component that implements CustomerDAO interface
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomer(Model theModel) {
+
+
+        List<Customer> theCustomers = customerDAO.getCustomers();
+
+        theModel.addAttribute("customers", theCustomers);
+
+
         return "list-customers";
     }
 }
---
+++ web-customer-tracker2/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,33 @@
+package com.luv2code.springdemo.dao;
+
+import com.luv2code.springdemo.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+
+        Session currrentSession = sessionFactory.getCurrentSession();
+
+        Query<Customer> theQuery = currrentSession.createQuery("from Customer", Customer.class);
+
+        List<Customer> customers = theQuery.getResultList();
+
+        return customers;
+    }
+}
+
+
